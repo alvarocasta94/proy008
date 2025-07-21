650ad8864ad333a7e28b67fc348db015be48e2c3
HeDichoQueNo <no@nomail.com>
2025-07-21 12:59:04 +0200

    Primer commit, inicio de proyecto Spring

---
+++ src/test/java/es/cic/curso2025/proy008/Proy008ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso2025.proy008;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proy008ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
