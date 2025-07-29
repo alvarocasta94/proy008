d3c56e48aaf1b1f0541e56f001694cbd718f2e84
HeDichoQueNo <no@nomail.com>
2025-07-29 12:55:24 +0200

    Arreglo declaracion de paquete

--- src/test/java/es/cic/curso2025/proy008/uc/SeCreaLibroDesdeEditorialIntegrationTest.java
+++ src/test/java/es/cic/curso2025/proy008/uc/SeCreaLibroDesdeEditorialIntegrationTest.java
@@ -1,3 +1,5 @@
+package es.cic.curso2025.proy008.uc;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
